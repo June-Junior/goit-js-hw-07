2c824342568a7aee7f01ba8309ea1d8017844c5d
June-Junior <artknownuntist@gmail.com>
2020-10-21 05:42:43 +0300

    commit 4

--- js/task-3.js
+++ js/task-3.js
@@ -16,10 +16,19 @@ const images = [
     },
   ];
 
-  const galleryMainList = document.querySelector("ul#gallery");
-
+const imagesNewArr = images.map(({url, alt}) => {
+    console.log(url);
+    console.log(alt);
+   return ({url, alt});
+})    
+console.log(imagesNewArr);
   
-  galleryMainList.insertAdjacentHTML('afterbegin',
-  `<li><img src="${images.url} alt="${images.alt}"></li>`
-);
+const galleryMainList = document.querySelector("#gallery");
 console.log(galleryMainList);
+
+  galleryMainList.insertAdjacentHTML('afterbegin',
+  `<li><img src="${imagesNewArr.url}" alt="${imagesNewArr.alt}"></li>`);
+console.log(galleryMainList);
+
+// 
+// '<li><img src="https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="White and Black Long Fur Cat"></li>'
